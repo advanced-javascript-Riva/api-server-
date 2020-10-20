797b6aa234e8184bf9aac343956c5ecf0e834ac6
RivaD2 <rivkadavidowski@gmail.com>
2020-10-20 16:28:01 -0700

    added routes folder and started server

--- index.js
+++ index.js
@@ -1,50 +1,27 @@
 'use strict';
 
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const app = ('express');
-const PORT = process.env.PORT || 3000;
+// const jsonServer = require('json-server');
+//const server = jsonServer.create();
+// const router = jsonServer.router('db.json');
+// const middlewares = jsonServer.defaults();
 
-server.use(middleware);
-server.use(router);
 
-server.listen(PORT, () => {
-    console.log('JSON server is up and running!')
-});
 
-app.get('/', (req, res) => {
+// server.use(middleware);
+// server.use(router);
 
-})
-//  Middleware: A function that executes when routes are being hit
-// app.use('posts', () => {
-// console.log('this is a middleware function for posts route ')
+const server = require('./lib/server.js');
+server.start();
+
+
+// app.use('post', () => {
+// console.log('this is a middleware function for post ')
 // });
-// //packages can be used with middleware
 
-//load supertest module
-//require express
-//npm install mongoose
-//use compass and atlas
-//get URI string, do not use localhost
-//import mongoose package
-//create .env file in VS code
-//install dotenv
-//in .env file(create it) add DB_CONNECTION: add string URI string
-//create PORT in .env
-//require dotenv package() with 'dotenv config'
-//Now I can change URI STRING
-//connect to db with mongoose.connect and add in URI to .env file
-//install nodemon
-// use newURL parser to true and other parser from notes lab
-//create folder called routes
-//add in route files and require express on each one
-// call const router = express.Router();
-//Now I can use router to create router
-// router.get, router.post, etc...
-//export the router module.exports = router;
-// Import routes in app.js:
+
+
+
+// Import routes from routes.js:
      //const PostRoute = require('./routes/posts) for example
      // create middleware = app.use('/posts', postRoute)
      // NOW, In posts.js under routes don't have to use postroute,
@@ -56,3 +33,4 @@ app.get('/', (req, res) => {
            // now again, I can just say app.use('/use', userRoute)
 /*In other words, just create route, export router and bring it
            into app.js and use it as a middleware*/
+
---
+++ routes/products.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const router = express.Router();
+
+
+
+
+
+
+router.post('/products', (req, res) => {
+
+})
+
+
+
+router.get('/products', (req, res) => {
+
+
+})
+router.get('/products/:id', (req, res) => {
+
+})
+
+
+
+router.put('/products/:id', (req, res) => {
+
+})
+
+module.exports = router;
