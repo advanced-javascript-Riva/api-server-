e6d497ea6f5a72b7e4b8583a67cf702074c83cd8
RivaD2 <rivkadavidowski@gmail.com>
2020-10-20 18:51:10 -0700

    set up relative routes for categories and products

--- routes/categories.js
+++ routes/categories.js
@@ -3,21 +3,21 @@ const express = require('express');
 const router = express.Router();
 
 // uses the object in the body of the request to create a new “record”
-router.post('/categories', (req, res) => {
+router.post('/', (req, res) => {
 
 })
 
 
 //getting all categories
-router.get('/categories', (req, res) => {
+router.get('/', (req, res) => {
 res.send('getting categories');
 
 })
 
 //get one category using id, this is a param so we can use params
-router.get('/categories/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     const categories = [1, 2, 3, 4];
-    const category = category.find(item => item.id === parseInt(req.params.id));
+    const category = categorues.find(item => item.id === parseInt(req.params.id));
     if(!category) return res.status(404).send('This category was not found');
     // if we do have a course with id, return it to client
     res.send(category);
@@ -27,12 +27,12 @@ router.get('/categories/:id', (req, res) => {
 
 //uses the object in the body to replace the record with the :id specified
 //updating all information using put
-router.put('/categories/:id', (req, res) => {
+router.put('/:id', (req, res) => {
 
 })
 
 //deletes the record with the :id specifiedr
-router.delete('/categories/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
 
 })
 module.exports = router;
--- routes/products.js
+++ routes/products.js
@@ -1,39 +1,39 @@
 'use strict'
 const express = require('express');
 const router = require('./categories');
-cons
+
 
 
 
 
 // uses the object in the body of the request to create a new “record”
-router.post('/products', (req, res) => {
+router.post('/', (req, res) => {
 
 })
 
 
 //get all categories
-router.get('/products', (req, res) => {
+router.get('/', (req, res) => {
     res.send([1, 2, 3]);
 
 })
 
 //get one category using id, this is a param so we can use params
-router.get('/products/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     res.send(req.params.id);
 })
 
 
 //uses the object in the body to replace the record with the :id specified
 //updating all information using put
-router.put('/products/:id', (req, res) => {
+router.put('/:id', (req, res) => {
 
 })
 
 
 
 //deletes the record with the :id specified
-router.delete('/products/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
 
 })
 
