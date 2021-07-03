3429849083929efdc6edb3f57d90284379c1f871
RivaD2 <rivkadavidowski@gmail.com>
2021-07-03 10:27:39 -0700

    search route addition

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const mongoose = require('mongoose');
 const server = require('./lib/server.js');
 server.start();
 
-mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser:true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser:true, useUnifiedTopology: true, })
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...',err));
-
-
-
-
