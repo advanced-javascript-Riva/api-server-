ea8fce4773b3f52b7d0421a6cab744f59266f547
RivaD2 <rivkadavidowski@gmail.com>
2022-01-22 10:49:07 -0800

    fixed bug for validation not creating user

--- middleware/validate.js
+++ middleware/validate.js
@@ -9,9 +9,7 @@ const validate = async (req, res, next) => {
   }
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    res.status(400).json({ errors: errors.array()})
-  } else if (errors.isEmpty()) {
-    res.status(200).json({success:true, message: "sign up is successful"})
+    res.status(400).json({ errors: errors.array()});
   } else {
     next();
   }
